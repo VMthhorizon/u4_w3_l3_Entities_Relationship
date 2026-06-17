d2251d62f388056e8be021987e5bfff0e162aef3
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-06-17 13:35:44 +0200

    Starting

--- src/main/java/vincenzomola/Application.java
+++ src/main/java/vincenzomola/Application.java
@@ -1,6 +1,12 @@
 package vincenzomola;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(
+            "u4_w3_l3_pu");
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
